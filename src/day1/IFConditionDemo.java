b43b177cb50ee77dc6c76ab32a268ad85ef976c8
uysolanki <uysolanki@gmail.com>
2026-07-23 13:22:16 +0530

    initial commit

---
+++ src/day1/IFConditionDemo.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class IFConditionDemo {
+
+	public static void main(String[] args) {
+	int age=21;
+	
+	if(age>=18)
+		System.out.println("Adult");
+	else
+		System.out.println("Minor");
+	}
+
+}
